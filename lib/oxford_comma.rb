da0e6eb69475f3cd700bf58fd37c5b1b413cf1e3
Alexander Urizar <urizaraj@gmail.com>
2018-02-08 23:02:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(' and ')
+  else
+    result = array[0..-2].join(', ')
+    result += ', and ' + array[-1]
+    return result
+  end
 end
